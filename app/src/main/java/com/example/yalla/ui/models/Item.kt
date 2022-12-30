b83b1dafb38a2995c752c8d9ad0dfe64c8164935
Assoulin <asulinisim@gmail.com>
2022-12-30 13:43:38 +0200

    Some changes made together

---
+++ app/src/main/java/com/example/yalla/ui/models/Item.kt
@@ -0,0 +1,9 @@
+package com.example.yalla.ui.models
+
+class Dish(
+    val dishName: String,
+    val description: String,
+    val posterURL: String,
+    val price: Double,
+    val category: String,
+)
